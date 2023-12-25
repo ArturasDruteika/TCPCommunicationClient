0c779f416d4fd9c96323b893a3c1d02a6e1f940d
ArtūrasDrūteika <arturas.druteika@gmail.com>
2023-12-25 21:31:45 +0200

    Updated main code

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,26 @@
 ﻿using ClientWrapper;
+using System.Reflection;
 
 class Program
 {
+    const string IP = "127.0.0.1";
+    const int PORT = 1024;
+
     static void Main(string[] args)
     {
-        const string IP = "127.0.0.1";
-        const int PORT = 1024;
+        DoSingleClientConnection();
+    }
+
+    private async static void DoSingleClientConnection()
+    {
+        string executablePath = Assembly.GetExecutingAssembly().Location;
+        string executableDirectory = Path.GetDirectoryName(executablePath);
+        string imgPath = Path.Combine(executableDirectory, "data", "images", "fish.jpg");
+
         Client client = new Client(IP, PORT);
-        client.Run("Hello Server from Client!!!");
+        await client.SendImgAsync(imgPath);
+
+        client.Close();
+        client.CloseStream();
     }
 }
