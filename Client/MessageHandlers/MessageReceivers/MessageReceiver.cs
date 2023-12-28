089fa98b74aadfab6994e6415465b108739f3cc9
ArtūrasDrūteika <arturas.druteika@gmail.com>
2023-12-28 19:57:25 +0200

    Implemented message receiver class with interface

---
+++ Client/MessageHandlers/MessageReceivers/MessageReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.MessageHandlers.MessageReceivers
+{
+    public class MessageReceiver : IMessageReceiver
+    {
+        public async Task ReceiveMsg(NetworkStream stream)
+        {
+            byte[] data = new byte[1024];
+            String responseData = String.Empty;
+
+            int bytes = await stream.ReadAsync(data, 0, data.Length);
+            responseData = Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: " + responseData);
+        }
+    }
+}
