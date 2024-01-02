2fce64b52040e17fc5774a6844b34ff6b6a6c0e5
ArtūrasDrūteika <arturas.druteika@gmail.com>
2024-01-02 16:07:58 +0200

    Updated program code to be more dynamic

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,21 +11,44 @@ class Program
 
     static void Main(string[] args)
     {
-        DoSingleClientConnection();
+        DoConnection();
     }
 
-    private async static void DoSingleClientConnection()
+    private async static void DoConnection()
     {
         ImageSender imageSender = new ImageSender();
         MessageSender messageSender = new MessageSender();
         MessageReceiver messageReceiver = new MessageReceiver();
 
         string executablePath = Assembly.GetExecutingAssembly().Location;
-        string executableDirectory = Path.GetDirectoryName(executablePath);
+        string? executableDirectory = Path.GetDirectoryName(executablePath);
         string imgPath = Path.Combine(executableDirectory, "data", "images", "fish.jpg");
 
-        AsyncTCPClient client = new AsyncTCPClient(IP, PORT, messageSender, imageSender, messageReceiver);
-        await client.SendImgAsync(imgPath);
+        await RunClient(messageSender, imageSender, messageReceiver);
+    }
+
+    private static async Task RunClient(IMessageSender messageSender, IImageSender imageSender, IMessageReceiver messageReceiver)
+    {
+        Console.WriteLine("Enter username: ");
+        string? username = Console.ReadLine();
+
+        AsyncTCPClient client = new AsyncTCPClient(IP, PORT, username, messageSender, imageSender, messageReceiver);
+
+        Console.WriteLine("Please enter some text:");
+
+        string? textInput = string.Empty;
+
+        while (true)
+        {
+            textInput = Console.ReadLine();
+
+            if (textInput == string.Empty)
+            {
+                break;
+            }
+
+            await client.SendMsgAsync(textInput);
+        }
 
         client.CloseConnection();
     }
