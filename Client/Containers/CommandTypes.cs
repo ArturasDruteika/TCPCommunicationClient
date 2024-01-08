a47cf7124c923d1e978b32d97d5744d4480b5803
ArtūrasDrūteika <arturas.druteika@gmail.com>
2024-01-08 20:54:33 +0200

    Removed unnec. namespaces

--- Client/Containers/CommandTypes.cs
+++ Client/Containers/CommandTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Client.Containers
+﻿namespace Client.Containers
 {
     public static class CommandTypes
     {
