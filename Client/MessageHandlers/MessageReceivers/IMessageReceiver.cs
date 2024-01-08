b8535a4cd18159cf5af6a2aeee84d3558d65fcc5
ArtūrasDrūteika <arturas.druteika@gmail.com>
2024-01-08 20:56:10 +0200

    Removed function returns
    Removed unnec. namespaces

--- Client/MessageHandlers/MessageReceivers/IMessageReceiver.cs
+++ Client/MessageHandlers/MessageReceivers/IMessageReceiver.cs
@@ -1,10 +1,9 @@
-﻿using AsyncTcpServer.Containers;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 
 namespace Client.MessageHandlers.MessageReceivers
 {
     public interface IMessageReceiver
     {
-        public Task<ClientStatus> ReceiveMsg(NetworkStream stream, CancellationToken ctsToken);
+        public void ReceiveMsg(NetworkStream stream, CancellationToken ctsToken);
     }
 }
--- Client/MessageHandlers/MessageReceivers/MessageReceiver.cs
+++ Client/MessageHandlers/MessageReceivers/MessageReceiver.cs
@@ -1,44 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
-using AsyncTcpServer.Containers;
 using Client.Containers;
 
 namespace Client.MessageHandlers.MessageReceivers
 {
     public class MessageReceiver : IMessageReceiver
     {
-        public async Task<ClientStatus> ReceiveMsg(NetworkStream stream, CancellationToken ctsToken)
+        public void ReceiveMsg(NetworkStream stream, CancellationToken ctsToken)
         {
             try
             {
                 byte[] buffer = new byte[1024];
                 int bytesRead;
 
-                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, ctsToken)) != 0)
+                while (!ctsToken.IsCancellationRequested)
                 {
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+
                     KeyValuePair<string, string> receivedData = ProcessReceivedData(buffer, bytesRead);
                     Console.WriteLine($"{receivedData.Key}: " + receivedData.Value);
                     Array.Clear(buffer, 0, buffer.Length);
                 }
-
-                if (bytesRead == 0)
-                {
-                    return ClientStatus.DISCONNECTED;
-                }
-
-                return ClientStatus.CONNECTED;
             }
-            catch (Exception ex)
+            catch (Exception ex) when (!(ex is OperationCanceledException))
             {
                 Console.WriteLine("Exception: " + ex.Message);
-                return ClientStatus.DISCONNECTED;
             }
         }
+
 
         private static KeyValuePair<string, string> ProcessReceivedData(byte[] buffer, int bytesRead)
         {
