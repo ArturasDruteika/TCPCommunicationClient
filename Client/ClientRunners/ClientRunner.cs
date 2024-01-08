11130f699fd7be7738b9710d30b73a8c85ff9126
ArtūrasDrūteika <arturas.druteika@gmail.com>
2024-01-08 21:14:32 +0200

    Implemented ClientRunner class which runs the client

---
+++ Client/ClientRunners/ClientRunner.cs
@@ -0,0 +1,42 @@
+﻿using Client.MessageHandlers.MessageReceivers;
+using Client.MessageHandlers.MessageSenders;
+using Client.ImageSenders;
+using Client.AsyncTCPClients;
+using System.Reflection;
+
+namespace Client.ClientRunners
+{
+    public class ClientRunner
+    {
+        const string IP = "127.0.0.1";
+        const int PORT = 1024;
+
+        public ClientRunner() 
+        {
+
+        }
+
+        public void Run() 
+        {
+            ImageSender imageSender = new ImageSender();
+            MessageSender messageSender = new MessageSender();
+            MessageReceiver messageReceiver = new MessageReceiver();
+
+            string executablePath = Assembly.GetExecutingAssembly().Location;
+            string? executableDirectory = Path.GetDirectoryName(executablePath);
+            string imgPath = Path.Combine(executableDirectory, "data", "images", "fish.jpg");
+
+            RunClient(messageSender, imageSender, messageReceiver);
+        }
+
+        private static void RunClient(IMessageSender messageSender, IImageSender imageSender, IMessageReceiver messageReceiver)
+        {
+            Console.WriteLine("Enter username: ");
+            string? username = Console.ReadLine();
+
+            AsyncTCPClient client = new AsyncTCPClient(IP, PORT, username, messageSender, imageSender, messageReceiver);
+            client.Run();
+        }
+
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,38 +1,10 @@
-﻿using Client.AsyncTCPClient;
-using Client.ImageSender;
-using Client.MessageHandlers.MessageReceivers;
-using Client.MessageHandlers.MessageSenders;
-using System.Reflection;
+﻿using Client.ClientRunners;
 
 class Program
 {
-    const string IP = "127.0.0.1";
-    const int PORT = 1024;
-
     static void Main(string[] args)
     {
-        DoConnection();
-    }
-
-    private async static void DoConnection()
-    {
-        ImageSender imageSender = new ImageSender();
-        MessageSender messageSender = new MessageSender();
-        MessageReceiver messageReceiver = new MessageReceiver();
-
-        string executablePath = Assembly.GetExecutingAssembly().Location;
-        string? executableDirectory = Path.GetDirectoryName(executablePath);
-        string imgPath = Path.Combine(executableDirectory, "data", "images", "fish.jpg");
-
-        RunClient(messageSender, imageSender, messageReceiver);
-    }
-
-    private static void RunClient(IMessageSender messageSender, IImageSender imageSender, IMessageReceiver messageReceiver)
-    {
-        Console.WriteLine("Enter username: ");
-        string? username = Console.ReadLine();
-
-        AsyncTCPClient client = new AsyncTCPClient(IP, PORT, username, messageSender, imageSender, messageReceiver);
-        client.Run();
+        ClientRunner clientRunner = new ClientRunner();
+        clientRunner.Run();
     }
 }
